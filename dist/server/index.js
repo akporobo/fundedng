7e0bf7a3c27b9acd8308332c35aebe6c620f9087
emprezito <emperorakporhiunu@gmail.com>
2026-07-11 22:59:21 +0100

    Remove dist/ from tracking - contains build artifacts with secrets

--- dist/server/index.js
+++
@@ -1,9 +0,0 @@
-import { a8, a9 } from "./assets/worker-entry-DS7H0w4O.js";
-import "node:events";
-import "node:async_hooks";
-import "node:stream/web";
-import "node:stream";
-export {
-  a8 as createServerEntry,
-  a9 as default
-};
